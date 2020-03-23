887f339df1e8e09483c22ead1e6d93832ad5d253
Bertrand Shao <shaob@g.ucla.edu>
2020-03-22 17:43:13 -0700

    finished rough draft of CSS

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const path = require('path')
-const db = require('./database/database.js')
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const db = require('./db/database.js');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')))
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`You are listening on port ${port}!`))
