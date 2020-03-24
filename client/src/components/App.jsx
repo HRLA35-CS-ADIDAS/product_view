6e9b97aea63b163a69c190ee234e723de56f2c63
Bertrand Shao <shaob@g.ucla.edu>
2020-03-23 18:39:55 -0700

    made minor changes to css

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import axios from 'axios';
+
+// class App extends React.Component {
+//     constructor(props){
+//       super(props);
+//       this.state = {
+//         products: [],
+//         product : {},
+//         sku: window.location.pathname.slice(1,-1)
+//       }
+//       this.getProducts  = this.getProducts.bind(this);
+//     }
+  
+//     componentDidMount(){
+//       this.getProducts();
+//     }
+
+// export default App;
